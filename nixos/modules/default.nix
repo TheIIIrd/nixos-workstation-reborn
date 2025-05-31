a34bfcf40778f3f56324d98f3c7b633692ae10ab
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2025-05-31 14:05:08 +0300

    Move files to base directory

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,35 +1,8 @@
 {
   imports = [
-    # ./amnezia.nix
-    ./audio.nix
-    ./bluetooth.nix
+    ./base
     ./boot
     ./desktop
-    ./env.nix
-    ./firefox.nix
-    # ./firejail.nix
-    ./flatpak.nix
-    ./fwupd.nix
     ./graphics
-    ./home-manager.nix
-    ./kernel.nix
-    ./locale.nix
-    ./mime.nix
-    ./net.nix
-    ./nh.nix
-    ./nix.nix
-    # ./obs.nix
-    ./printing.nix
-    # ./spice.nix
-    ./steam.nix
-    ./timezone.nix
-    # ./tlp.nix
-    ./trim.nix
-    ./user.nix
-    ./virt-manager.nix
-    # ./yggdrasil.nix
-    ./zapret.nix
-    ./zerotierone.nix
-    ./zram.nix
   ];
 }
